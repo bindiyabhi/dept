13d066827e0483d7b0eea3788e67e370ae2d57bc
bindiyabhi <79558186+bindiyabhi@users.noreply.github.com>
2021-03-15 09:49:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Computer Science</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-success">
+        <div class="container-fluid">
+            <a class="navbar-brand" target="_blank" href="https://rajagiri.edu/">
+                <img src="images\Rajagiri Logo-small.jpg" alt="" width="30" height="30">
+             </a>
+            <a class="navbar-brand" href="rajagiri.php">Rajagiri</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Computer Science</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Laboratories</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Current Batches
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">MCA</a></li>
+                            <li><a class="dropdown-item" href="#">MSC</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">About Us</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+        </nav>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6"> 
+            <table class="table ">
+                <tr>
+                    <td>Username</td>
+                    <td><input type="text" class="text"></td>
+                </tr>
+                <tr>
+                    <td>Password       </td>
+                    <td><input type="text" class="text"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="button" value=" Submit"></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+</script>
+</body>
+</html>
---
+++ rajagiri.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Computer Science</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-success">
+                <div class="container-fluid">
+                     <a class="navbar-brand" target="_blank" href="index.php">
+                    <img src="images\Rajagiri Logo-small.jpg" alt="" width="30" height="30">
+                    </a>
+                    <a class="navbar-brand" href="rajagiri.php">Rajagiri</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Computer Science</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Laboratories</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Current Batches
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">MCA</a></li>
+                                    <li><a class="dropdown-item" href="#">MSC</a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">About Us</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        
+        
+        </div>
+        <div class="row">
+            <div class="col col-12 col-xs-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+                <p class="text-justify fs-6">
+                    Rajagiri College of Social Sciences (RCSS), together with its sister educational concerns, owes its existence to the CMI (Carmelites of Mary Immaculate) fathers, the first ever indigenous religious congregation for men in the Syrian Catholic tradition of Christianity in India. The CMIs drawing inspiration from their founding father St. Kuriakose Elias Chavara, a great visionary, reformer and religious leader of the 19th century, have proven themselves worthy of that heritage in the field of education by establishing institutions of excellence imparting quality education, across the length and breadth of the State, and in various parts of India.
+                </p>
+            </div>
+            <div class="col">
+            <img src="images\rajagiri-1.jpg" class="img-fluid" alt="Rajagiri Hill Campus">
+            
+            </div>
+        </div> 
+        <div class="row">
+        <div class="col col-xs-12 col-sm-6 col-lg-6 col xl-6 col-xxl-6">
+            <div class="accordion" id="accordionExample">
+                 <div class="accordion-item">
+                     <h2 class="accordion-header" id="headingOne">
+                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Departments
+                    </button>
+                     </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        <strong>
+                            <ul>
+                                <li>Social Work</li>
+                                <li>Business Administration</li>
+                                <li>Personnel Management</li>
+                                <li>Computer Science</li>
+                                <li>Library and Information Science</li>
+                                <li>Commerce and Professional Studies</li>
+                                <li>Psychology</li>
+                                <li>Statistics</li>
+                                <li>Bio Sciences</li>
+                            </ul>
+                            </strong> 
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        NAAC Accreditations 
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <strong>
+                            <ul>
+                                <li>Accreditations
+                                    <ul>
+                                        <li>2000: 5 *</li>
+                                        <li>2007: A+</li>
+                                        <li>2013: A with CGPA 3.7/4.00</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            </strong> 
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo">
+                        NIRF Rankings
+                    </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <strong>
+                            <ul>
+                                <li>Accreditations
+                                    <ul>
+                                        <li>2017: 48</li>
+                                        <li>2018: 35</li>
+                                        <li>2019: 28 </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            </strong> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+                     
+        </div>
+    
+    
+    
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"> </script>
+</body>
+</html>
